a2537c9254764fe151c13e36bd4e37e6c16ac48e
PanibratovR3 <roman.panibratov@gmail.com>
2025-04-09 13:22:09 +0300

    Added hovering effects for cells.

--- Script.js
+++ Script.js
@@ -1,16 +1,22 @@
-function drawGird(size) {
+function drawGrid(size) {
   let screen = document.querySelector(".sketch-screen");
   for (let i = 0; i < size; i++) {
     let column = document.createElement("div");
-    // column.classList.add("column");
+    column.classList.add("column");
     for (let j = 0; j < size; j++) {
       let row = document.createElement("div");
-      //   row.classList.add("row");
+      row.classList.add("row");
       row.style.border = "2px solid black";
+      row.addEventListener("mouseenter", (event) =>
+        event.target.classList.toggle("hover")
+      );
+      row.addEventListener("mouseleave", (event) => {
+        event.target.classList.toggle("default");
+      });
       column.appendChild(row);
     }
     screen.appendChild(column);
   }
 }
 
-drawGird(7);
+drawGrid(10);
